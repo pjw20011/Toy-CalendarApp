6e834706b56ed995210dc0007a084c29cc07870d
Jeongwon Park <pjw200116@gmail.com>
2025-01-31 17:36:52 +0900

    firstcommit

---
+++ src/main/java/com/example/toycalendarapp/ToyCalendarAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.toycalendarapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyCalendarAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyCalendarAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/toycalendarapp/ToyCalendarAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toycalendarapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyCalendarAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
