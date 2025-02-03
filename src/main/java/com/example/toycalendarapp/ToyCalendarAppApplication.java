7c4e9e380686e3c43562f82f1609ded4d190df8d
Jeongwon Park <pjw200116@gmail.com>
2025-02-03 17:39:51 +0900

    ãinput CsvFile

--- src/main/java/com/example/toycalendarapp/ToyCalendarAppApplication.java
+++ src/main/java/com/example/toycalendarapp/ToyCalendarAppApplication.java
@@ -1,42 +1,23 @@
 package com.example.toycalendarapp;
 
 import com.example.toycalendarapp.event.*;
-import org.springframework.boot.SpringApplication;
+import com.example.toycalendarapp.reader.EventCsvReader;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.ZonedDateTime;
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class ToyCalendarAppApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Schedule schedule = new Schedule();
-        HashSet<String> participants = new HashSet<>();
-        participants.add("박정원");
-        Meeting meeting1 = new Meeting(
-                1,"meeting1", ZonedDateTime.now(), ZonedDateTime.now().plusHours(1),
-                participants, "MeetingRoomA", "스터디"
-        );
 
-        schedule.add(meeting1);
+        EventCsvReader csvReader = new EventCsvReader();
+        String meetingCsvPath = "/data/meeting.csv";
 
-        Todo todo1 = new Todo(
-                2, "todo1", ZonedDateTime.now().plusHours(3), ZonedDateTime.now().plusHours(4),
-                "할 일 적기"
-        );
-        schedule.add(todo1);
-
-        Todo todo2 = new Todo(
-                3, "todo2", ZonedDateTime.now().plusHours(5), ZonedDateTime.now().plusHours(4),
-                "할 일 적기"
-        );
-        schedule.add(todo2);
-
-     //   list.forEach(Event::print);
+        List<Meeting> meetings = csvReader.readMeetings(meetingCsvPath);
+        meetings.forEach(each -> schedule.add(each));
 
         schedule.printAll();
        // SpringApplication.run(ToyCalendarAppApplication.class, args);
---
+++ src/main/java/com/example/toycalendarapp/reader/EventCsvReader.java
@@ -0,0 +1,71 @@
+package com.example.toycalendarapp.reader;
+
+import com.example.toycalendarapp.event.Meeting;
+import com.opencsv.CSVReader;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public class EventCsvReader {
+    public List<Meeting> readMeetings(String path) throws IOException{
+        List<Meeting> result = new ArrayList<>();
+
+        // 데이터를 읽는 부분
+        List<String[]> read = readAll(path);
+        for(int i= 0; i< read.size(); i++){
+            if (skipHeader(i)) {
+                continue;
+            }
+
+            String[] each = read.get(i);
+
+            //Meeting 으로 변환하는 부분
+            result.add(
+                    new Meeting(
+                            Integer.parseInt(each[0]), // ID
+                            each[2], // 제목
+                            ZonedDateTime.of(
+                                    LocalDateTime.parse(
+                                            each[6].trim(), // 앞뒤 공백 제거
+                                            DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")), // T 포함된 형식
+                                    ZoneId.of("Asia/Seoul")),
+                            ZonedDateTime.of(
+                                    LocalDateTime.parse(
+                                            each[7].trim(), // 앞뒤 공백 제거
+                                            DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")), // T 포함된 형식
+                                    ZoneId.of("Asia/Seoul")),
+                            new HashSet<>(Arrays.asList(each[3].split(","))), // 참가자 리스트
+                            each[4], // 장소
+                            each[5]  // 설명
+                    )
+            );
+
+        }
+
+
+        return result;
+    }
+
+    private boolean skipHeader(int i) {
+        return i == 0;
+    }
+
+    private List<String[]> readAll(String path) throws IOException {
+        InputStream in = getClass().getResourceAsStream(path);
+        InputStreamReader reader = new InputStreamReader(in, StandardCharsets.UTF_8);
+
+        CSVReader csvReader = new CSVReader(reader);
+        return csvReader.readAll();
+    }
+
+}
